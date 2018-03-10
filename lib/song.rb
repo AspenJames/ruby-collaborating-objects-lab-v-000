9e176c377ab0f73697a4e155b7a8584302e2115a
Aspen James <aspenjames@tqca.org>
2018-03-10 00:38:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,6 +14,7 @@ class Song
   def self.new_by_filename(filename)
     song = Song.new(filename.split(" - ")[1])
     self.artist_name=(filename.split(" - ")[0])
+    song.artist = filename.split(" - ")[0]
     binding.pry
     song
   end
